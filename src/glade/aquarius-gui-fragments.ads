6a37c95196bb85f995dff169b4d92b3690905450
Fraser Wilson <blancolioni@gmail.com>
2012-04-19 17:22:30 +0200

    added background fragment colour

--- src/glade/aquarius-gui-fragments.ads
+++ src/glade/aquarius-gui-fragments.ads
@@ -1,7 +1,9 @@
 private with Ada.Strings.Unbounded;
 private with Gtk.Text_Buffer;
 
+with Aquarius.Fonts;
 with Aquarius.Rendering;
+
 with Gtk.Widget;
 
 package Aquarius.GUI.Fragments is
@@ -11,6 +13,13 @@ package Aquarius.GUI.Fragments is
    function Title (Fragment : Root_Fragment_Type'Class) return String;
    procedure Set_Title (Fragment : in out Root_Fragment_Type'Class;
                         Text     : in     String);
+
+   function Background
+     (Fragment : Root_Fragment_Type'Class)
+      return Aquarius.Fonts.Aquarius_Colour;
+
+   procedure Set_Background (Fragment : in out Root_Fragment_Type'Class;
+                             Background : Aquarius.Fonts.Aquarius_Colour);
 
    function Create_Widget
      (Fragment : Root_Fragment_Type)
@@ -31,6 +40,7 @@ private
       record
          Width, Height : Positive;
          Title         : Ada.Strings.Unbounded.Unbounded_String;
+         Background    : Aquarius.Fonts.Aquarius_Colour;
          Text_Buffer   : Gtk.Text_Buffer.Gtk_Text_Buffer;
          Renderer      : Aquarius.Rendering.Aquarius_Renderer;
       end record;
