f71d1578b343266824f75fe02b34b4dfade63cb0
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2012-04-19 16:40:34 +0200

    initial fragment GUI

---
+++ src/glade/aquarius-gui-fragments.adb
@@ -0,0 +1,101 @@
+with Glib;
+with Gtk.Enums;
+with Gtk.Text_View;
+
+with Aquarius.Rendering.GUI;
+with Aquarius.Styles;
+
+package body Aquarius.GUI.Fragments is
+
+   type Note_Fragment_Type is
+     new Root_Fragment_Type with
+      record
+         Text : Ada.Strings.Unbounded.Unbounded_String;
+      end record;
+
+   overriding
+   procedure Render (Fragment : in out Note_Fragment_Type);
+
+   --------------------------
+   -- Create_Note_Fragment --
+   --------------------------
+
+   function Create_Note_Fragment
+     (Width, Height : in     Positive;
+      Initial_Text  : in     String)
+      return Aquarius_Fragment
+   is
+      Fragment : constant Aquarius_Fragment :=
+                   new Note_Fragment_Type
+                     '(Root_Fragment_Type with
+                       Text => Ada.Strings.Unbounded.To_Unbounded_String
+                         (Initial_Text));
+   begin
+      Gtk.Text_Buffer.Gtk_New (Fragment.Text_Buffer);
+      Fragment.Width := Width;
+      Fragment.Height := Height;
+      Fragment.Renderer :=
+        Aquarius.Rendering.GUI.New_GUI_Renderer
+          (Fragment.Text_Buffer);
+      Fragment.Renderer.Set_Style
+        (Aquarius.Styles.Default_Style);
+      return Fragment;
+   end Create_Note_Fragment;
+
+   -------------------
+   -- Create_Widget --
+   -------------------
+
+   function Create_Widget
+     (Fragment : Root_Fragment_Type)
+      return Gtk.Widget.Gtk_Widget
+   is
+      Text_View : Gtk.Text_View.Gtk_Text_View;
+   begin
+      Gtk.Text_View.Gtk_New (Text_View);
+      Text_View.Set_Size_Request (Width  => Glib.Gint (Fragment.Width),
+                                  Height => Glib.Gint (Fragment.Height));
+      Text_View.Set_Buffer (Fragment.Text_Buffer);
+      Text_View.Set_Wrap_Mode (Wrap_Mode => Gtk.Enums.Wrap_Word);
+      Text_View.Show;
+      return Gtk.Widget.Gtk_Widget (Text_View);
+   end Create_Widget;
+
+   ------------
+   -- Render --
+   ------------
+
+   overriding
+   procedure Render (Fragment : in out Note_Fragment_Type) is
+      R : constant Aquarius.Rendering.Aquarius_Renderer := Fragment.Renderer;
+   begin
+      R.Begin_Render;
+      R.Set_Text
+        (Position => (1, 1),
+         Class    => "normal",
+         Text     => Ada.Strings.Unbounded.To_String (Fragment.Text));
+      R.End_Render;
+   end Render;
+
+   ---------------
+   -- Set_Title --
+   ---------------
+
+   procedure Set_Title
+     (Fragment : in out Root_Fragment_Type'Class;
+      Text     : in     String)
+   is
+   begin
+      Fragment.Title := Ada.Strings.Unbounded.To_Unbounded_String (Text);
+   end Set_Title;
+
+   -----------
+   -- Title --
+   -----------
+
+   function Title (Fragment : Root_Fragment_Type'Class) return String is
+   begin
+      return Ada.Strings.Unbounded.To_String (Fragment.Title);
+   end Title;
+
+end Aquarius.GUI.Fragments;
