115029a81264a0c453d2f2635550ff75f6b58d00
Fraser Wilson <blancolioni@gmail.com>
2012-04-23 14:53:11 +0200

    putting task stop at end of main program

--- src/glade/aquarius-gui.adb
+++ src/glade/aquarius-gui.adb
@@ -24,7 +24,6 @@ with Aquarius.GUI.Views;
 with Aquarius.Fragments.Notes;
 
 with Aquarius.Buffers;
-with Aquarius.Tasks;
 
 package body Aquarius.GUI is
 
@@ -159,8 +158,6 @@ package body Aquarius.GUI is
       end;
 
       Gtk.Main.Main;
-
-      Aquarius.Tasks.Stop;
 
    end Launch_GUI;
 
