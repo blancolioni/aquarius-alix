4cc0340ecae99ccda337ece387d91934dcd0bbc7
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2012-05-14 13:31:53 +0200

    added Function_Specification after String_Literal

--- src/plugins/ada/ada_plugin-ch06.ads
+++ src/plugins/ada/ada_plugin-ch06.ads
@@ -15,6 +15,9 @@ package Ada_Plugin.Ch06 is
    procedure Procedure_Spec_After_Defining_Name
      (Procedure_Spec, Procedure_Name  : Program_Tree);
 
+   procedure Function_Spec_After_Defining_String_Literal
+     (Function_Spec, String_Literal  : Program_Tree);
+
    procedure Procedure_Declaration_After
      (Procedure_Declaration : Program_Tree);
 
