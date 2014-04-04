394409e92ff1451fd44937b2c2ce771fe8689294
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2014-04-04 20:59:16 +0200

    Separate Aquarius.Colours package

---
+++ src/composition/aquarius-colours.adb
@@ -0,0 +1,77 @@
+with Aquarius.Configuration;
+
+package body Aquarius.Colours is
+
+   -----------
+   -- Black --
+   -----------
+
+   function Black return Aquarius_Colour is
+   begin
+      return Parse_Colour ("black");
+   end Black;
+
+   ----------
+   -- Blue --
+   ----------
+
+   function Blue  (Colour : Aquarius_Colour) return Colour_Range is
+   begin
+      return Colour.Blue;
+   end Blue;
+
+   -----------
+   -- Green --
+   -----------
+
+   function Green (Colour : Aquarius_Colour) return Colour_Range is
+   begin
+      return Colour.Green;
+   end Green;
+
+   ----------------
+   -- Hex_Colour --
+   ----------------
+
+   function Hex_Colour (Item : Colour_Range) return String is
+      Hex_Digit : constant array (Colour_Range range 0 .. 15) of Character :=
+        "0123456789ABCDEF";
+
+   begin
+      return (Hex_Digit (Item / 16), Hex_Digit (Item mod 16));
+   end Hex_Colour;
+
+   ------------------
+   -- Parse_Colour --
+   ------------------
+
+   function Parse_Colour (Colour_Spec : String) return Aquarius_Colour is
+      use Aquarius.Configuration;
+      Position : constant Cursor := Get_Cursor ("/colours/" & Colour_Spec);
+      R, G, B  : Integer;
+   begin
+      R := Get_Value (Position, "r");
+      G := Get_Value (Position, "g");
+      B := Get_Value (Position, "b");
+      return (Colour_Range (R), Colour_Range (G), Colour_Range (B));
+   end Parse_Colour;
+
+   ---------
+   -- Red --
+   ---------
+
+   function Red   (Colour : Aquarius_Colour) return Colour_Range is
+   begin
+      return Colour.Red;
+   end Red;
+
+   -----------
+   -- White --
+   -----------
+
+   function White return Aquarius_Colour is
+   begin
+      return Parse_Colour ("white");
+   end White;
+
+end Aquarius.Colours;
---
+++ src/composition/aquarius-colours.ads
@@ -0,0 +1,24 @@
+package Aquarius.Colours is
+
+   type Aquarius_Colour is private;
+   type Colour_Range is range 0 .. 255;
+
+   function Parse_Colour (Colour_Spec : String) return Aquarius_Colour;
+
+   function Red   (Colour : Aquarius_Colour) return Colour_Range;
+   function Green (Colour : Aquarius_Colour) return Colour_Range;
+   function Blue  (Colour : Aquarius_Colour) return Colour_Range;
+
+   function Hex_Colour (Item : Colour_Range) return String;
+
+   function Black return Aquarius_Colour;
+   function White return Aquarius_Colour;
+
+private
+
+   type Aquarius_Colour is
+      record
+         Red, Green, Blue : Colour_Range;
+      end record;
+
+end Aquarius.Colours;
