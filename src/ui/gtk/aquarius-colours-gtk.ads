c75d56b7e6704a01b8932b178d372867aa1dc420
Fraser Wilson <fwilson@anago.nl>
2014-04-09 11:51:54 +0200

    UI: Added mouse button press handler for overview

--- src/ui/gtk/aquarius-colours-gtk.ads
+++ src/ui/gtk/aquarius-colours-gtk.ads
@@ -1,9 +1,14 @@
 with Gdk.Color;
+with Gdk.RGBA;
 
 package Aquarius.Colours.Gtk is
 
    function To_Gdk_Color
      (Colour : Aquarius_Colour)
       return Gdk.Color.Gdk_Color;
+
+   function To_Gdk_RGBA
+     (Colour : Aquarius_Colour)
+      return Gdk.RGBA.Gdk_RGBA;
 
 end Aquarius.Colours.Gtk;
