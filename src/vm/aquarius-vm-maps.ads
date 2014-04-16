30c5332a3b0d66b752a5463fb93124e9a57a3b3a
Fraser Wilson <fwilson@anago.nl>
2014-04-16 10:15:13 +0200

    VM: Added string to VM_Value map property type, and library function support

---
+++ src/vm/aquarius-vm-maps.ads
@@ -0,0 +1,54 @@
+private with Ada.Containers.Hashed_Maps;
+private with Ada.Strings.Unbounded.Hash;
+
+package Aquarius.VM.Maps is
+
+   type Map_Property_Type is
+     new Root_Aquarius_Object with private;
+
+   overriding function Name
+     (Item : Map_Property_Type)
+      return String;
+
+   procedure Insert
+     (Property : in out Map_Property_Type'Class;
+      Key      : in     String;
+      Value    : in     VM_Value);
+
+   procedure Replace
+     (Property : in out Map_Property_Type'Class;
+      Key      : in     String;
+      Value    : in     VM_Value);
+
+   function Contains
+     (Property : Map_Property_Type'Class;
+      Key      : String)
+      return Boolean;
+
+   function Element
+     (Property : Map_Property_Type'Class;
+      Key      : String)
+      return VM_Value;
+
+   procedure Iterate
+     (Property : Map_Property_Type'Class;
+      Process  : not null access
+        procedure (Key : String;
+                   Value : VM_Value));
+
+private
+
+   package VM_Value_Maps is
+     new Ada.Containers.Hashed_Maps
+       (Key_Type        => Ada.Strings.Unbounded.Unbounded_String,
+        Element_Type    => VM_Value,
+        Hash            => Ada.Strings.Unbounded.Hash,
+        Equivalent_Keys => Ada.Strings.Unbounded."=");
+
+   type Map_Property_Type is
+     new Root_Aquarius_Object with
+      record
+         Map : VM_Value_Maps.Map;
+      end record;
+
+end Aquarius.VM.Maps;
