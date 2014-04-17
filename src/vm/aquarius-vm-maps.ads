2425f8e75822ef5c815435076a9be79385766e02
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2014-04-17 17:39:32 +0200

    VM.Maps: added version of Iterate that does not involve Keys.  Iterate now preserves order.

--- src/vm/aquarius-vm-maps.ads
+++ src/vm/aquarius-vm-maps.ads
@@ -1,4 +1,5 @@
 private with Ada.Containers.Hashed_Maps;
+private with Ada.Containers.Doubly_Linked_Lists;
 private with Ada.Strings.Unbounded.Hash;
 
 package Aquarius.VM.Maps is
@@ -36,19 +37,30 @@ package Aquarius.VM.Maps is
         procedure (Key : String;
                    Value : VM_Value));
 
+   procedure Iterate
+     (Property : Map_Property_Type'Class;
+      Process  : not null access
+        procedure (Value : VM_Value));
+
 private
+
+   package VM_Value_Lists is
+     new Ada.Containers.Doubly_Linked_Lists
+       (Element_Type    => VM_Value);
 
    package VM_Value_Maps is
      new Ada.Containers.Hashed_Maps
        (Key_Type        => Ada.Strings.Unbounded.Unbounded_String,
-        Element_Type    => VM_Value,
+        Element_Type    => VM_Value_Lists.Cursor,
         Hash            => Ada.Strings.Unbounded.Hash,
-        Equivalent_Keys => Ada.Strings.Unbounded."=");
+        Equivalent_Keys => Ada.Strings.Unbounded."=",
+        "="             => VM_Value_Lists."=");
 
    type Map_Property_Type is
      new Root_Aquarius_Object with
       record
-         Map : VM_Value_Maps.Map;
+         Map  : VM_Value_Maps.Map;
+         List : VM_Value_Lists.List;
       end record;
 
 end Aquarius.VM.Maps;
