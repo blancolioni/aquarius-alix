5a3a041e0521cc21e22399cba47934c2073e1d39
Fraser Wilson <fwilson@anago.nl>
2015-01-30 10:11:53 +0100

    New library option: Show_Paths_In_Messages if true, show the (relative) path to the file mentioned in the message, otherwise just the file name

--- src/driver/aquarius-library.ads
+++ src/driver/aquarius-library.ads
@@ -1,9 +1,11 @@
 package Aquarius.Library is
 
    procedure Initialise
-     (Enable_Plugins : Boolean := True);
+     (Enable_Plugins         : Boolean := True;
+      Show_Paths_In_Messages : Boolean := False);
 
    function Plugins_Enabled return Boolean;
+   function Show_Paths_In_Messages return Boolean;
 
    procedure Enable_Plugins (Enabled : Boolean);
 
