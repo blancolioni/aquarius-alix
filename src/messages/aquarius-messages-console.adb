eafd02794a7d2a9b27141d793b7e9909a84a2b6d
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2015-06-26 17:11:03 +0200

    Console messages: added function Check_Messages, which checks a Message_Location for messages equal or more serious than a given Message_Level, writes any messages found to Standard_Error, and returns the most serious level found, or No_Message if there are no matching messages.

--- src/messages/aquarius-messages-console.adb
+++ src/messages/aquarius-messages-console.adb
@@ -2,6 +2,28 @@ with Ada.Text_IO;
 
 package body Aquarius.Messages.Console is
 
+   --------------------
+   -- Check_Messages --
+   --------------------
+
+   function Check_Messages
+     (Source : Message_Location'Class;
+      Minimum : Message_Level := Warning)
+      return Message_Level
+   is
+      List : Message_List;
+      Highest : Message_Level := No_Message;
+   begin
+      Source.Get_Messages (List);
+      if Message_Count (List) > 0 then
+         Highest := Highest_Level (List);
+         if Highest >= Minimum then
+            Aquarius.Messages.Console.Show_Messages (List);
+         end if;
+      end if;
+      return Highest;
+   end Check_Messages;
+
    -------------------
    -- Show_Messages --
    -------------------
--- src/messages/aquarius-messages-console.ads
+++ src/messages/aquarius-messages-console.ads
@@ -2,4 +2,12 @@ package Aquarius.Messages.Console is
 
    procedure Show_Messages (List : in Message_List);
 
+   function Check_Messages
+     (Source : Message_Location'Class;
+      Minimum : Message_Level := Warning)
+      return Message_Level;
+   --  check Source for messages, and report any with a level > Minimum.
+   --  return the highest message level found.
+   --  if no messages found, No_Message is returned.
+
 end Aquarius.Messages.Console;
