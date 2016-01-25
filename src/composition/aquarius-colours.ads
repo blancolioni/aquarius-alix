8c21b6761b80bba18be1e34c18cd602d0c5c5572
Fraser Wilson <blancolioni@gmail.com>
2016-01-25 16:46:29 +0100

    Aquarius colours are now simply strings representing the colour. Setting default font for text view.

--- src/composition/aquarius-colours.ads
+++ src/composition/aquarius-colours.ads
@@ -1,28 +1,33 @@
+private with Aquarius.Names;
+
 package Aquarius.Colours is
 
    type Aquarius_Colour is private;
-   type Colour_Range is range 0 .. 255;
 
-   function Parse_Colour (Colour_Spec : String) return Aquarius_Colour;
+   function From_String
+     (String_Spec : String)
+      return Aquarius_Colour;
 
-   function Red   (Colour : Aquarius_Colour) return Colour_Range;
-   function Green (Colour : Aquarius_Colour) return Colour_Range;
-   function Blue  (Colour : Aquarius_Colour) return Colour_Range;
+   function From_RGB
+     (R, G, B     : Natural)
+      return Aquarius_Colour;
 
-   function Hex_Colour (Item : Colour_Range) return String;
+   function From_RGBA
+     (R, G, B, A  : Natural)
+      return Aquarius_Colour;
 
    function Black return Aquarius_Colour;
    function White return Aquarius_Colour;
 
-   function From_RGB
-     (R, G, B : Aquarius.Colours.Colour_Range)
-      return Aquarius.Colours.Aquarius_Colour;
+   function To_String
+     (Colour : Aquarius_Colour)
+      return String;
 
 private
 
    type Aquarius_Colour is
       record
-         Red, Green, Blue : Colour_Range;
+         Text : Aquarius.Names.Aquarius_Name;
       end record;
 
 end Aquarius.Colours;
