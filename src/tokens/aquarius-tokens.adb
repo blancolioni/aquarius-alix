24ea00fa48b311c82c846a8601494607e2bde912
Fraser Wilson <blancolioni@gmail.com>
2016-01-29 09:02:43 +0100

    Aquarius.Tokens: added missing Have_Class initialisation

--- src/tokens/aquarius-tokens.adb
+++ src/tokens/aquarius-tokens.adb
@@ -561,6 +561,7 @@ package body Aquarius.Tokens is
       Unique := True;
       Tok := Null_Token;
       Class := Null_Token_Class;
+      Have_Class := False;
 
       while First <= Text'Last and then Text (First) = ' ' loop
          First := First + 1;
