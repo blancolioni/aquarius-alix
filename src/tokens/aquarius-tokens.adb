b88b715b61996d493a223d580a7a600621c4a889
Fraser Wilson <blancolioni@gmail.com>
2016-07-06 21:12:38 +0200

    Reverted change which skipped new lines, because these can be significant.

--- src/tokens/aquarius-tokens.adb
+++ src/tokens/aquarius-tokens.adb
@@ -564,9 +564,7 @@ package body Aquarius.Tokens is
       Have_Class := False;
       Last := 0;
 
-      while First <= Text'Last and then
-        (Text (First) = ' ' or else Character'Pos (Text (First)) = 10)
-      loop
+      while First <= Text'Last and then Text (First) = ' ' loop
          First := First + 1;
       end loop;
 
