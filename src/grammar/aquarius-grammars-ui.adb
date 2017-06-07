2fd360087bb096563f287439c0d414e8277e07fd
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2017-06-07 15:37:29 +0200

    Aquarius.Grammars.UI: no longer need to scan plugin trees ourselves, just get the action program from the grammar.

--- src/grammar/aquarius-grammars-ui.adb
+++ src/grammar/aquarius-grammars-ui.adb
@@ -1,36 +1,20 @@
-with Ada.Directories;
 with Ada.Text_IO;
-with Ada.Strings.Unbounded;
 
 with Komnenos.Entities;
-
-with Aquarius.Config_Paths;
-with Aquarius.Loader;
 
 with Aquarius.Programs.Komnenos_Entities;
 with Aquarius.Syntax.Komnenos_Entities;
 
 package body Aquarius.Grammars.UI is
 
-   function Grammar_Path
-     (Grammar : Aquarius_Grammar;
-      Name    : String)
-      return String
-   is (Aquarius.Config_Paths.Config_File
-       ("grammar/" & Grammar.Name & "/"
-        & (if Name (Name'First) = '.'
-           then Grammar.Name & Name
-           else Name)));
-
-   procedure Bind_Plugin_Actions
+   procedure Bind_Grammar_Actions
      (Target  : Komnenos.UI.Komnenos_UI;
-      Grammar : Aquarius_Grammar;
-      Plugin  : Aquarius.Programs.Program_Tree);
+      Grammar : Aquarius_Grammar);
 
    procedure Bind_Action_File
      (Target      : Komnenos.UI.Komnenos_UI;
       Grammar     : Aquarius_Grammar;
-      Group_Name  : String;
+      Group       : Aquarius.Actions.Action_Group;
       Action_File : Aquarius.Programs.Program_Tree);
 
    ----------------------
@@ -40,7 +24,7 @@ package body Aquarius.Grammars.UI is
    procedure Bind_Action_File
      (Target      : Komnenos.UI.Komnenos_UI;
       Grammar     : Aquarius_Grammar;
-      Group_Name  : String;
+      Group       : Aquarius.Actions.Action_Group;
       Action_File : Aquarius.Programs.Program_Tree)
    is
       Actions : constant Aquarius.Trees.Array_Of_Trees :=
@@ -66,7 +50,7 @@ package body Aquarius.Grammars.UI is
         (Header : Aquarius.Programs.Program_Tree)
          return String
       is (Action_Entity_Key
-          (Group_Name    => Group_Name,
+          (Group_Name    => Aquarius.Actions.Action_Group_Name (Group),
            Position_Name =>
               Header.Program_Child ("action_time").Concatenate_Children,
            Parent_Name   =>
@@ -127,67 +111,43 @@ package body Aquarius.Grammars.UI is
                               Entity_Spec      => Definition,
                               Entity_Body      => null);
          begin
---              Ada.Text_IO.Put_Line ("action: " & Entity.Name
---                                    & " [" & Key & "]");
+            Ada.Text_IO.Put_Line ("action: " & Entity.Name
+                                  & " [" & Entity.Key & "]");
             Grammar.Action_Entities.Insert
               (Key, Entity);
          end;
       end loop;
    end Bind_Action_File;
 
-   -------------------------
-   -- Bind_Plugin_Actions --
-   -------------------------
+   --------------------------
+   -- Bind_Grammar_Actions --
+   --------------------------
 
-   procedure Bind_Plugin_Actions
+   procedure Bind_Grammar_Actions
      (Target  : Komnenos.UI.Komnenos_UI;
-      Grammar : Aquarius_Grammar;
-      Plugin  : Aquarius.Programs.Program_Tree)
+      Grammar : Aquarius_Grammar)
    is
 
-      procedure Bind (Group : Aquarius.Programs.Program_Tree);
+      procedure Bind
+        (Group          : Aquarius.Actions.Action_Group;
+         Action_Program : Aquarius.Programs.Program_Tree);
 
       ----------
       -- Bind --
       ----------
 
-      procedure Bind (Group : Aquarius.Programs.Program_Tree) is
+      procedure Bind
+        (Group          : Aquarius.Actions.Action_Group;
+         Action_Program : Aquarius.Programs.Program_Tree)
+      is
       begin
-         for Tree of
-           Group.Children ("list_of_actions/action_file_reference")
-         loop
-            declare
-               use Ada.Strings.Unbounded;
-               Name : Unbounded_String := Null_Unbounded_String;
-            begin
-               for Id of Tree.Children ("identifier") loop
-                  if Name /= Null_Unbounded_String then
-                     Name := Name & "-" & Id.Text;
-                  else
-                     Name := To_Unbounded_String (Id.Text);
-                  end if;
-               end loop;
-
-               Bind_Action_File
-                 (Target, Grammar,
-                  Group.Program_Child ("identifier").Text,
-                  Aquarius.Loader.Load_From_File
-                    (Grammar_Path (Grammar, To_String (Name) & ".action")));
-
-            end;
-         end loop;
+         Bind_Action_File
+           (Target, Grammar, Group, Action_Program);
       end Bind;
 
    begin
-      for Tree of
-        Plugin.Children
-          ("source_file/plugin_declaration/list_of_declarations/declaration/"
-           & "group_declaration")
-      loop
-         Bind (Aquarius.Programs.Program_Tree (Tree));
-      end loop;
-
-   end Bind_Plugin_Actions;
+      Grammar.Scan_Action_Programs (Bind'Access);
+   end Bind_Grammar_Actions;
 
    ------------------
    -- Load_Grammar --
@@ -203,23 +163,7 @@ package body Aquarius.Grammars.UI is
            (Target, Grammar.Name, Non_Terminal);
       end loop;
 
-      declare
-         use type Aquarius.Programs.Program_Tree;
-         Plugin_Path : constant String :=
-                         Grammar_Path (Grammar, ".plugin");
-         Plugin      : constant Aquarius.Programs.Program_Tree :=
-                         (if Ada.Directories.Exists (Plugin_Path)
-                          then Aquarius.Loader.Load_From_File (Plugin_Path)
-                          else null);
-      begin
-         if Plugin = null then
-            Ada.Text_IO.Put_Line
-              (Ada.Text_IO.Standard_Error,
-               "warning: " & Grammar.Name & " has no plugin");
-         else
-            Bind_Plugin_Actions (Target, Grammar, Plugin);
-         end if;
-      end;
+      Bind_Grammar_Actions (Target, Grammar);
 
    end Load_Grammar;
 
