a49c0d600c86419f9eb6af1eb6932c2e64570f1a
Fraser Wilson <blancolioni@gmail.com>
2017-07-17 11:38:24 +0200

    Ack: loop instruction stub

--- src/ack/aquarius-ack-generate.adb
+++ src/ack/aquarius-ack-generate.adb
@@ -277,6 +277,8 @@ package body Aquarius.Ack.Generate is
                null;
             when N_Conditional =>
                null;
+            when N_Loop =>
+               null;
             when N_Precursor =>
                Generate_Precursor (Unit, Instruction);
          end case;
