2d57cdd32b76a77b1e4f35cafa4af7b821fc41c0
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2017-07-24 08:30:00 +0200

    Ack: moved binding action generation to Ack.Bindings

---
+++ src/ack/ack-bindings.ads
@@ -0,0 +1,17 @@
+with Aqua.Images;
+
+with Aquarius.Actions;
+with Aquarius.Grammars;
+
+package Ack.Bindings is
+
+   function Load_Ack_Binding
+     (Action_File_Path : String;
+      Base_Aqua_Path   : String;
+      Image            : Aqua.Images.Image_Type;
+      Grammar          : Aquarius.Grammars.Aquarius_Grammar;
+      Group            : Aquarius.Actions.Action_Group;
+      Trigger          : Aquarius.Actions.Action_Execution_Trigger)
+      return Boolean;
+
+end Ack.Bindings;
