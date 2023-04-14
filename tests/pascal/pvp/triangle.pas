53e5a164aa2f1fd061c0a15b65f63015d40d7d12
Fraser Wilson <fwilson@anago.nl>
2023-04-14 11:51:49 +0200

    Update triangle.pas

--- tests/pascal/pvp/triangle.pas
+++ tests/pascal/pvp/triangle.pas
@@ -1,8 +1,10 @@
 program triangle (input, output);
-var a, b, c, s, area : real;
+var
+   a, b, c, s, area : real;
 begin
-read(a, b, c);
-s := 0.5 * (a+b+c);
-area := sqrt(s*(s-a)*(s-b)*(s-c));
-write(area)
+   read (a, b, c);
+   s := 0.5 * (a + b + c);
+   area := sqrt (s * (s - a) * (s - b) * (s - c));
+area
+   (area)
 end.
